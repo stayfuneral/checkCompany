48e320e3eb021078f7b306c8d45966814d923b1a
Roman <stay.funeral@gmail.com>
2020-02-03 12:44:23 +0700

    Update install.php

--- install.php
+++ install.php
@@ -3,20 +3,25 @@
 require_once __DIR__ . '/app/crest.php'; // use class CRest from Bitrix24 SDK
 
 $result = CRest::installApp();
-$eventBind = CRest::callBatch([
-	'set_event_onCrmCompanyAdd' => [
-		'method' => 'event.bind',
-		'params' => [
-			'event' => 'onCrmCompanyAdd',
-			'handler' => 'https://your.domain/eventHandler.php' // Event handler URL
-		]
-	],
-	'set_event_onCrmCompanyUpdate' => [
-		'method' => 'event.bind',
-		'params' => [
-			'event' => 'onCrmCompanyUpdate',
-			'handler' => 'https://your.domain/eventHandler.php' // Event handler URL
-		]
-	]
-]);
+
+$events = [];
+$eventList = CRest::call('event.get')['result'];
+
+foreach($eventList as $event) {
+	$events[] = $event['event'];
+}
+
+if(!in_array('ONCRMCOPMANYADD', $events)) {
+	CRest::call('event.bind', [
+		'event' => 'onCrmCompanyAdd'
+		'handler' => 'https://bx.kraska54.ru/events.php'
+	]);
+}
+
+if(!in_array('ONCRMCOMPANYUPDATE', $events)) {
+	CRest::call('event.bind', [
+		'event' => 'onCrmCompanyUpdate'
+		'handler' => 'https://bx.kraska54.ru/events.php'
+	]);
+}
 
